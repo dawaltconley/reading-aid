30906752309e6a012a145b28e0af8925cf5b1ac1
Dylan Awalt-Conley <web@dylan.ac>
2022-01-21 12:31:13 -0500

    added babel plugin for private class methods; make class properties plugin explicit

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,9 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      ['@babel/plugin-proposal-class-properties', { loose: true }],
+      ['@babel/plugin-proposal-private-methods', { loose: true }],
+    ],
   };
 };
